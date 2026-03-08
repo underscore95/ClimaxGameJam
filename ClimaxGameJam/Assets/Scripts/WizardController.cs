9611506ca3f756db847614367a4c205a8f7e5fcb
underscore95 <107675637+underscore95@users.noreply.github.com>
2026-03-08 10:35:09 +0000

    Fire balls

--- ClimaxGameJam/Assets/Scripts/WizardController.cs
+++ ClimaxGameJam/Assets/Scripts/WizardController.cs
@@ -7,14 +7,20 @@ using Random = UnityEngine.Random;
 public class WizardController : MonoBehaviour
 {
     [SerializeField] private EntityHealth _wizardHealth;
-    [SerializeField] private ObjectPoolMonoBehaviour _pool;
+    [SerializeField] private ObjectPoolMonoBehaviour _ghostPool;
+    [SerializeField] private ObjectPoolMonoBehaviour _fireballPool;
     [SerializeField] private int _maxGhosts = 3;
-    [SerializeField] private float _minSpawnInterval = 2.5f;
-    [SerializeField] private float _maxSpawnInterval = 4.5f;
+    [SerializeField] private float _minSpawnIntervalGhosts = 2.5f;
+    [SerializeField] private float _maxSpawnIntervalGhosts = 4.5f;
+    [SerializeField] private float _minSpawnIntervalFireballs = 1.5f;
+    [SerializeField] private float _maxSpawnIntervalFireballs = 3.5f;
+    private Collider _playerCollider;
     private readonly List<GhostController> _ghosts = new();
+    private readonly List<Fireball> _fireballs = new();
 
     private void Awake()
     {
+        _playerCollider = FindFirstObjectByType<PlayerController>().gameObject.GetComponent<Collider>();
         _wizardHealth.OnDeath += () =>
         {
             while (_ghosts.Count > 0)
@@ -26,6 +32,7 @@ public class WizardController : MonoBehaviour
         };
 
         StartCoroutine(StartGhostSpawner());
+        StartCoroutine(StartFireballSpawner());
         Debug.Assert(GetComponentInParent<Room>());
     }
 
@@ -36,7 +43,7 @@ public class WizardController : MonoBehaviour
 
     private IEnumerator StartGhostSpawner()
     {
-        yield return new WaitForSeconds(Random.Range(_minSpawnInterval, _maxSpawnInterval));
+        yield return new WaitForSeconds(Random.Range(_minSpawnIntervalGhosts, _maxSpawnIntervalGhosts));
         if (_ghosts.Count < _maxGhosts)
         {
             SpawnGhost();
@@ -45,13 +52,28 @@ public class WizardController : MonoBehaviour
         StartCoroutine(StartGhostSpawner());
     }
 
+    private IEnumerator StartFireballSpawner()
+    {
+        yield return new WaitForSeconds(Random.Range(_minSpawnIntervalFireballs, _maxSpawnIntervalFireballs));
+        _fireballPool.Pool.ActivateObject(obj =>
+        {
+            Fireball fireball = obj.GetComponent<Fireball>();
+            fireball.Wizard = this;
+            obj.transform.position = transform.position;
+            obj.transform.forward = Vector3.Normalize(_playerCollider.bounds.center - transform.position);
+            _fireballs.Add(fireball);
+        });
+
+        StartCoroutine(StartFireballSpawner());
+    }
+
     private void SpawnGhost()
     {
         Vector3 position = transform.position + transform.forward * 0.75f;
 
-        Debug.Assert(_pool);
-        Debug.Assert(_pool.Pool != null);
-        _pool.Pool.ActivateObject(obj =>
+        Debug.Assert(_ghostPool);
+        Debug.Assert(_ghostPool.Pool != null);
+        _ghostPool.Pool.ActivateObject(obj =>
         {
             Debug.Assert(obj);
             obj.transform.position = position;
@@ -68,6 +90,14 @@ public class WizardController : MonoBehaviour
         Debug.Assert(ghostController.Wizard == this);
         ghostController.Wizard = null;
         _ghosts.Remove(ghostController);
-        _pool.Pool.ReleaseObject(ghostController.gameObject);
+        _ghostPool.Pool.ReleaseObject(ghostController.gameObject);
+    }
+
+    internal void RemoveFireball(Fireball fireball)
+    {
+        Debug.Assert(fireball.Wizard == this);
+        fireball.Wizard = null;
+        _fireballs.Remove(fireball);
+        _fireballPool.Pool.ReleaseObject(fireball.gameObject);
     }
 }
