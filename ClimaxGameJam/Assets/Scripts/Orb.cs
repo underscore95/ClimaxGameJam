22ab5848a5fd3104c432adcf778efcbc1f00f9f8
underscore95 <107675637+underscore95@users.noreply.github.com>
2026-03-08 14:10:42 +0000

    win state

---
+++ ClimaxGameJam/Assets/Scripts/Orb.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Orb : MonoBehaviour
+{
+    [SerializeField] private InputActionReference _interactInput;
+    [SerializeField] private float _fadeTime = 1.0f;
+    [SerializeField] private float _displayTime = 5f;
+    [SerializeField] private float _interactDistance = 15f;
+    [SerializeField] private CanvasGroup _winCanvas;
+    [SerializeField] private Canvas _interactCanvas;
+    private PlayerController _player;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        Debug.Assert(_winCanvas);
+        Debug.Assert(_fadeTime > 0);
+        _player = FindFirstObjectByType<PlayerController>();
+        _interactInput.action.Enable();
+        _camera = Camera.main;
+        _interactCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        bool pressed = _interactInput.action.WasPressedThisFrame();
+        if (_player.Dead || _player.Won)
+        {
+            return;
+        }
+
+        _interactCanvas.enabled = Physics.Raycast(
+            _camera.transform.position,
+            _camera.transform.forward,
+            out _,
+            _interactDistance,
+            1 << gameObject.layer
+        );
+
+        if (!pressed || !_interactCanvas.enabled) return;
+
+        StartCoroutine(Win());
+    }
+
+    private IEnumerator Win()
+    {
+        _player.Won = true;
+
+        yield return CanvasUtils.FadeIn(_winCanvas, _fadeTime);
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        yield return GameObjectUtils.DestroyAllExcept(gameObject, _camera.gameObject, _winCanvas.gameObject);
+
+        yield return new WaitForSeconds(_displayTime);
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
---
+++ ClimaxGameJam/Assets/Scripts/Utils/CanvasUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasUtils
+{
+    public static IEnumerator FadeIn(CanvasGroup group, float duration)
+    {
+        float t = 0f;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            group.alpha = t / duration;
+            yield return null;
+        }
+
+        group.alpha = 1f;
+    }
+}
