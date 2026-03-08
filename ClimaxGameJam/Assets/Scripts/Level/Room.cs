b11b874b7abbcf67582d0b875e2c2923d15d45af
underscore95 <107675637+underscore95@users.noreply.github.com>
2026-03-08 08:55:49 +0000

    Added doors to prefabs

--- ClimaxGameJam/Assets/Scripts/Level/Room.cs
+++ ClimaxGameJam/Assets/Scripts/Level/Room.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    [SerializeField] private GameObject _door;
+
     public int EnemiesAlive { get; internal set; }
+    private List<Vector3> _spawnPositions = new();
 
     private void Awake()
     {
-        EnemiesAlive = GetComponentsInChildren<WizardController>().Length;
-        
+        foreach (Spawner spawner in GetComponentsInChildren<Spawner>())
+        {
+            EnemiesAlive++;
+            _spawnPositions.Add(spawner.transform.position);
+            Destroy(spawner);
+        }
+
         foreach (Transform child in transform)
         {
             child.gameObject.AddComponent<MeshCollider>().sharedMesh =
---
+++ ClimaxGameJam/Assets/Scripts/Level/Spawner.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public sealed class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+
+    public GameObject Prefab => _prefab;
+}
