35efdff1c02c1bceeaefd0fbc3ea93dcc44b3879
underscore95 <107675637+underscore95@users.noreply.github.com>
2026-03-08 13:33:39 +0000

    attack vfx implemented

--- ClimaxGameJam/Assets/Scripts/Fireball.cs
+++ ClimaxGameJam/Assets/Scripts/Fireball.cs
@@ -6,14 +6,17 @@ public class Fireball : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _velocity = 10;
     [SerializeField] private float _damage = 1;
+    [SerializeField] private LayerMask _ignoreLayers;
     public WizardController Wizard { get; internal set; }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!Wizard) return;
+        if ((_ignoreLayers & 1 << other.gameObject.layer) != 0) return;
+        Wizard.RemoveFireball(this);
         if (!other.gameObject.TryGetComponent<PlayerController>(out _)) return;
         EntityHealth health = other.gameObject.GetComponent<EntityHealth>();
         health.Health -= _damage;
-        Wizard.RemoveFireball(this);
     }
 
     private void FixedUpdate()
